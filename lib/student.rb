1f1c3dc1f8fb957061c5d097b9b8e1d3ae5d66b5
aurel fontaine <ajfame11@gmail.com>
2020-06-03 18:41:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -116,11 +116,17 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    s = self.first_X_students_in_grade_10(1)
-    s.map do |i|
-      self.new_from_db(i)
-    end[0]
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end.first
   end
+
 
 
   def self.all_students_in_grade_X(grade)
