327d190ac125a9cf2827c31b129764d124fa35d7
aurel fontaine <ajfame11@gmail.com>
2020-06-03 18:42:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -107,28 +107,6 @@ class Student
     end.first
   end
 
-
-=======
-       WHERE students.grade = 10
-    SQL
-
-    DB[:conn].execute(sql, x)
-  end
-
-  def self.first_student_in_grade_10
-    sql = <<-SQL
-      SELECT * FROM students
-      WHERE grade = 10
-      LIMIT 1
-    SQL
-
-    DB[:conn].execute(sql).collect do |row|
-      self.new_from_db(row)
-    end.first
-  end
-
-
-
   def self.all_students_in_grade_X(grade)
     sql = <<-SQL
        SELECT * FROM students WHERE grade = ?
